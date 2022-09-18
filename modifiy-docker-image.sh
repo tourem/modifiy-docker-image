7335709417fafef520b321d77b7d28b6937ad01b
Mahamadou TOURE <touremohameda@gmail.com>
2022-09-18 16:12:47 +0200

    Update modifiy-docker-image.sh

--- modifiy-docker-image.sh
+++ modifiy-docker-image.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+
+############################################################
+# Docker create command: creates a fresh new container from a docker image. However, it doesn’t run it immediately. 
+#
+# Docker start command: will start any stopped container. If you used docker create command to create a container, you can start it with this command.
+#
+# Docker run command: is a combination of create and start as it creates a new container and starts it immediately. In fact, 
+# the docker run command can even pull an image from Docker Hub if it doesn’t find the mentioned image on your system
+#                                                 #
+############################################################
+
 docker stop akhq_config_containe
 docker rm akhq_config_containe
 winpty docker create --name akhq_container tchiotludo/akhq:latest
-docker cp akhq/application.yml akhq_container:/app
+docker cp application-akhq.yml akhq_container:/app
 docker rmi tchiotludo/akhq-config:latest
 docker commit akhq_container tchiotludo/akhq-config:latest
 docker stop akhq_container
